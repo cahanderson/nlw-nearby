fedee90b07f0eebf29b56b80baa60be70e4809d8
Carlos Anderson <cahanderson@gmail.com>
2024-12-11 21:05:13 -0300

    commit parcial aula 2 1:10

---
+++ src/app/home.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
+import { api } from "@/services/api";
+import { Categories, CategoriesProps } from "@/components/Categories";
+
+export default function Home(){
+    const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
+
+    async function fetchCategories(){
+        try{
+            const {data} = await api.get("/categories")
+            setCategories(data)
+            setCategory(data[0].id)
+        }catch(error){
+            // console.log(error)
+            Alert.alert("Categorias","Não foi possível carregar as categorias.")
+        }
+    }
+    useEffect(()=>{
+        fetchCategories()
+    },[])
+
+    return(
+        <View style={{flex:1}}>
+            <Categories data={categories} onSelect={setCategory} selected={category}/>
+        </View>
+    )
+}
